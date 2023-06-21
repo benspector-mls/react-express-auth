ae4d658d2016b030928cc61b17b5eb288dd783a0
benspector-mls <ben@marcylabschool.org>
2023-06-21 15:10:34 -0400

    adding production setup

--- knexfile.js
+++ knexfile.js
@@ -21,13 +21,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.CONNECTION_STRING || {
-      host: process.env.PG_HOST || '127.0.0.1',
-      port: process.env.PG_PORT || 5432,
-      user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'postgres',
-      database: process.env.PG_DB || 'react_auth_example',
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
       stub: path.join(__dirname, 'migration-stub.js'),
