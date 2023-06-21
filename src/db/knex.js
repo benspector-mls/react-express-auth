60b12d480fa3834a686dccf0cb3a2723c0bb20e1
benspector-mls <ben@marcylabschool.org>
2023-06-21 14:01:14 -0400

    env

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,3 +1,4 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('../../knexfile')[env];
+console.log('environment', env);
+const config = require('../../knexfile')['development'];
 module.exports = require('knex')(config);
