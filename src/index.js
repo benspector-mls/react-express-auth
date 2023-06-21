f786a9c020f67ab000cdc4c24f6fe1e0a4a1c520
benspector-mls <ben@marcylabschool.org>
2023-06-21 14:26:25 -0400

    remove host

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const server = require('./server');
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+  console.log(`Server running`);
 });
